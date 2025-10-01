9d428f08f8573bf13adbabe35830f30b74d4aff2
HuiBeom <gmlqja7913@daum.net>
2025-10-01 23:43:51 +0900

    recoiltodo 복습

--- example1/src/components/Nav.jsx
+++ example1/src/components/Nav.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav>
-            <h1 className='bg-orange-100 w-full py-1'>hi</h1>
+            <h1 className='bg-neutral-700 w-full py-1'>hi</h1>
+            <div className='columns-2xl px-4'>
+                <ul className='flex gap-6'>
+                    <li className='hover:underline cursor-pointer '>Home</li>
+                    <li className='hover:underline cursor-pointer '>About</li>
+                    <li className='hover:underline cursor-pointer '>Services</li>
+                    <li className='hover:underline cursor-pointer '>Contact</li>
+                </ul>
+            </div>
         </nav>
     );
 };
