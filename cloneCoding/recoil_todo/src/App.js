d329027b5732c65d5933a9301585cfbc4bae541a
HuiBeom <gmlqja7913@daum.net>
2025-10-02 15:02:41 +0900

    recoil docs 읽고 따라 해보기

--- cloneCoding/recoil_todo/src/App.js
+++ cloneCoding/recoil_todo/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {TodoListStats} from "./components/TodoListStats";
+import {TodoListFilters} from "./components/TodoListFilters";
+import {TodoItemCreator} from "./components/TodoItemCreator";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
+        <TodoList />
+        {/*<TodoListStats />*/}
+        {/*<TodoListFilters />*/}
+        {/*<TodoItemCreator />*/}
 
     </div>
   );
--- cloneCoding/recoil_todo/src/todoAtom.js
+++ cloneCoding/recoil_todo/src/todoAtom.js
@@ -1,7 +1,29 @@
-import {atom} from "recoil"
+import {atom, selector, useRecoilValue} from "recoil"
 
 export const todoListState = atom({
     key: 'todoListState',
     default: []
 });
 
+export const todoListFilterState = atom({
+    key: 'todoListFilterState',
+    default: 'Show All',
+});
+
+export const todoListStatsState = selector({
+    key: 'todoListStatsState',
+    get: ({get}) => {
+        const todoList = get(todoListState);
+        const totalNum = todoList.length;
+        const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+        const totalUncompletedNum = totalNum - totalCompletedNum;
+        const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+        return {
+            totalNum,
+            totalCompletedNum,
+            totalUncompletedNum,
+            percentCompleted,
+        };
+    },
+});
