5af2eb5bd8f68faeb465be0d53c778d98cd641c7
HuiBeom <gmlqja7913@daum.net>
2025-10-12 15:01:36 +0900

    grid 연습 반응형 디자인 연습

---
+++ example3/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="grid-container">
+            <div className="item item1">
+                <h3>Card 1</h3>
+                <p>설명:...</p>
+            </div>
+            <div className="item item2">
+                <h3>Card 2</h3>
+                <p>설명:...</p>
+            </div>
+            <div className="item item3">
+                <h3>Card 3</h3>
+                <p>설명:...</p>
+            </div>
+            <div className="item item4">
+                <h3>Card 4</h3>
+                <p>설명:...</p>
+            </div>
+            <div className="item item5">
+                <h3>Card 5</h3>
+                <p>설명:...</p>
+            </div>
+            <div className="item item6">
+                <h3>Card 6</h3>
+                <p>설명:...</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
